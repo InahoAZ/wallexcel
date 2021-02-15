67208bb33d6d2497493ace981bf9858694903521
Gabriel Britez <gbritez53@gmail.com>
2021-02-15 01:02:51 -0300

    Estructura Basica React y Pagina de Inicio creada

---
+++ wallexcel_front/src/pages/SignIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignIn = () => {
+	return (
+		<>
+			<h1>Perfil del usuario...</h1>
+		</>
+	);
+};
+
+export default SignIn;
---
+++ wallexcel_front/src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUp = () => {
+	return (
+		<>
+			<h1>Página de Registro de Usuario...</h1>
+		</>
+	);
+};
+
+export default SignUp;
